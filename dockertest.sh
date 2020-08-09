09e792a78722e0a538c7b683622a8883375a8744
Bob Robey <brobey@lanl.gov>
2020-08-09 08:54:14 -0600

    more container cleanup

--- dockertest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker build --no-cache -t chapter4 .
-#docker run -it --entrypoint /bin/bash chapter4
-docker build --no-cache -f Dockerfile.Ubuntu20.04 -t chapter4 .
-#docker run -it --entrypoint /bin/bash chapter4
-docker build --no-cache -f Dockerfile.debian -t chapter4 .
-#docker run -it --entrypoint /bin/bash chapter4
